3054073a47666f4be77f9a64ddc7bd1c5ba9039c
stlouisweb <stlouisweb@gmail.com>
2017-04-30 08:59:08 -0500

    working on dynamic transaction account list WIP.

--- src/Components/TransactionForm.js
+++ src/Components/TransactionForm.js
@@ -1,60 +1,96 @@
-import React, {Component} from "react"
-import Form from "muicss/lib/react/form"
-import Input from "muicss/lib/react/input"
-import Textarea from "muicss/lib/react/textarea"
-import Button from "muicss/lib/react/button"
-import Checkbox from "muicss/lib/react/checkbox"
-import Col from "muicss/lib/react/col"
-import Row from "muicss/lib/react/row"
-import Option from "muicss/lib/react/option"
-import Select from "muicss/lib/react/select"
+import React, {Component} from "react";
+import Form from "muicss/lib/react/form";
+import Input from "muicss/lib/react/input";
+import Button from "muicss/lib/react/button";
+import Col from "muicss/lib/react/col";
+import Row from "muicss/lib/react/row";
+import Option from "muicss/lib/react/option";
+import Select from "muicss/lib/react/select";
 
 class TransactionForm extends Component {
-	handleAddAcctClick() {
-		alert("click")
-	}
-
-	render() {
-		let Accounts = this.props.accounts.filter(acct => acct.label !== "Cash")
+  constructor(props) {
+    super(props);
+    this.state = {
+      accounts: [{label: "Cash", value: 0}, {label: "Sales", value: 0}],
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+  handleAddAcctClick(e) {
+    e.preventDefault();
+    alert("click");
+  }
+  handleInputChange(e) {
+    var accts = this.state.accounts;
+    var acct = accts.findIndex(acct => acct.label === e.target.id);
+    console.log(acct);
+    var val = accts[acct].value.toString() + e.target.value.toString();
+    accts[acct].value = val;
+    console.log(accts);
+    this.setState({accounts: accts});
+  }
+  render() {
+    let Accounts = this.props.accounts.filter(acct => acct.label !== "Cash");
     //	Accounts.splice(0, 1)
-		let AccountOptions = Accounts.map((acct, i) => (
+    let AccountOptions = Accounts.map((acct, i) => (
       <Option value={acct.label} label={acct.label} key={i.toString()} />
-    ))
-		return (
+    ));
+    let transactionAccounts = this.state.accounts.filter(
+      acct => acct.label !== "Cash",
+    );
+    let AcctRows = transactionAccounts.map((acct, i) => (
+      <Row key={i}>
+        <Col md="5">
+          <Input
+            floatingLabel={true}
+            label="Amount"
+            id={acct.label}
+            value={acct.value > 0 ? acct.value : ""}
+            onChange={this.handleInputChange}
+          />
+        </Col>
+        <Col md="7">
+          <Select defaultValue={acct.label}>
+            {AccountOptions}
+          </Select>
+        </Col>
+      </Row>
+    ));
+    return (
       <div id="transactionForm" className="appPanel">
         <h2>Add Transaction</h2>
         <Form style={{height: "450px", overflowY: "scroll"}}>
 
-          <Input type="date" hint="Date" />
+          <Input type="date" label="Date" />
           <h4>Accounts</h4>
           <Row>
             <Col md="5">
-              <Input hint="Amount" />
+              <Input floatingLabel={true} label="Amount" />
             </Col>
             <Col md="7">
-              <label>Cash</label>
+              <aside>Cash</aside>
             </Col>
           </Row>
-          <Row>
+          {AcctRows}
+          {/*<Row>
             <Col md="5">
-              <Input hint="Amount" />
+              <Input floatingLabel={true} label="Amount" />
             </Col>
             <Col md="7">
               <Select defaultValue="Sales">
                 {AccountOptions}
               </Select>
             </Col>
-          </Row>
+          </Row> */}
           <div className="mui--clearfix" />
           <Button variant="raised" onClick={this.handleAddAcctClick}>
             Add Account
           </Button>
-          <Input hint="Description" />
+          <Input floatingLabel={true} label="Description" />
           <Button variant="raised">Submit Transaction</Button>
         </Form>
       </div>
-		)
-	}
+    );
+  }
 }
 
-export default TransactionForm
+export default TransactionForm;
